c7fbb5fc0320ed34a39b35dcd990e3e2fc5e9f9d
Your Name <junjxiao@163.com>
2025-05-27 12:04:29 +0800

    add submodules

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ conda install pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=
 conda install -c iopath iopath
 conda install pytorch3d -c pytorch3d
 pip install -r requirement.txt
-pip install git+https://github.com/graphdeco-inria/diff-gaussian-rasterization/
-pip install git+https://github.com/AllenXiangX/SnowflakeNet/models/pointnet2_ops_lib
+pip install ./submodules/diff-gaussian-rasterization/
+pip install ./submodules/pointnet2_ops_lib/
